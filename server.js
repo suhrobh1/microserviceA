fd26b4bd3ff2b3b6e74ed9ba58b8a408ea7d703c
suhrobh1 <93023422+suhrobh1@users.noreply.github.com>
2025-05-01 22:39:27 -0700

    intial

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios"); // Import the axios library for making HTTP requests
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+app.post("/bmi_fetch", async (req, res) => {
+    const { age, gender, height, weight } = req.body;
+    console.log("Received:", age, gender, height, weight);
+
+  if (age && height && gender && weight){
+    if(gender == "male"){
+        let bmi = (weight / ( height ** 2)) * 703
+        res.json(`Male BMI was requested: ${bmi}` )
+    }else{
+        let bmi = (weight / ( height ** 2)) * 703
+        res.json(`Female BMI was requested: ${bmi}`)
+    }
+  }else{
+    res.status(400).json({ error: "Age, gender, weight, and height are all required!" });
+  }
+});
+
+
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "OK", timestamp: new Date().toISOString() });
+});
+
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, () => {
+  console.log(`BMI microservice running on port ${PORT}`);
+});
